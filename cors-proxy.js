2cfb0987528b2f9b82a6bbb86357898f78dfccf1
Tom MacWright <tom@macwright.org>
2013-06-27 11:11:50 -0400

    Add dumb caching and basic authentication

--- cors-proxy.js
+++ cors-proxy.js
@@ -2,15 +2,26 @@
 
 var request = require('request'),
     cors = require('cors'),
+    argv = require('optimist').argv,
     express = require('express');
 
 var app = express();
 
-app.get('/', cors(), function(req, res){
+var dumbCache = {};
+
+app.get('/', cors(), function(req, res) {
+    if (argv.cache && dumbCache[req.query.url]) {
+        return res.send(dumbCache[req.query.url]);
+    }
     request({
         uri: req.query.url,
-        headers: req.query.headers ? JSON.parse(req.query.headers) : {}
+        headers: req.query.headers ? JSON.parse(req.query.headers) : {},
+        auth: req.query.auth ? JSON.parse(req.query.auth) : null
     }, function(err, resp, body) {
+        if (argv.cache) dumbCache[req.query.url] = body;
+        res.set({
+            'Content-Type': 'application/xml'
+        });
         res.send(body);
     });
 });
